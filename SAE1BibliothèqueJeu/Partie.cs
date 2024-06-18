77d2db39ee1dbb91c076742f8da402b8d5bcaceb
maryam <mimiboubrit25@gmail.com>
2024-06-18 21:58:41 +0200

    Creation d'une classe partie (pseudo)

--- SAE1BibliothèqueJeu/Partie.cs
+++ SAE1BibliothèqueJeu/Partie.cs
@@ -1,7 +1,21 @@
-﻿namespace SAE1BibliothèqueJeu
+﻿namespace SAE1BibliothequeJeu
 {
+
     public class Partie
     {
+        private string _pseudoJoueur;
+        public Partie(string PseudoJoueur)
+        {
+            _pseudoJoueur = PseudoJoueur;
+        }
 
+        public string PseudoJoueur
+        {
+            get
+            {
+                return _pseudoJoueur;
+            }
+        }
     }
+
 }
